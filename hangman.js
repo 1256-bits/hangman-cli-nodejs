31088d5ddbdf63172a6192c0844eaed28f4a09b2
= <=>
2022-09-20 18:12:55 +0300

    Add checking for used letters

--- hangman.js
+++ hangman.js
@@ -1,16 +1,21 @@
 function main() {
-    const prompt = require("prompt-sync")({sigint: true});
+    const prompt = require("prompt-sync")({ sigint: true });
     const word = "hangman";
     let lives = 5;
     const used = [];
     while (lives > 0) {
-        const input = prompt("Letter: ").trim().toLowerCase().slice(0,1);
+        const input = prompt("Letter: ").trim().toLowerCase().slice(0, 1);
         if (!/[a-z]/.test(input)) {
             console.log("Invalid input.");
             continue;
         }
+        if(used.includes(input)) {
+            console.log(`${input} is already used`);
+            continue;
+        }
         if (word.includes(input)) {
             console.log(`Correct. Lives: ${lives}`);
+            used.push(input);
         }
         else {
             lives--;
