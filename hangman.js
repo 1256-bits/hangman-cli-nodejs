122a70ff9108b74282fcfc1bac17541635545a19
= <=>
2022-09-20 22:34:00 +0300

    Add winning condition

--- hangman.js
+++ hangman.js
@@ -5,27 +5,37 @@ function main() {
     let won = false;
     let lives = 5;
     const used = [];
+    let displayWord = Object.values(wordOut).join("");
     while (lives > 0) {
-        console.log(Object.values(wordOut).join(""));
+        console.log(displayWord);
         const input = prompt("Letter: ").trim().toLowerCase().slice(0, 1);
+
         if (!/[a-z]/.test(input)) {
             console.log("Invalid input.");
             continue;
         }
+
         if (used.includes(input)) {
             console.log(`${input} is already used`);
             continue;
         }
+
         if (word.includes(input)) {
             console.log(`Correct. Lives: ${lives}`);
             used.push(input);
             for (let i of findIndexes(word, input)) {
                 wordOut[i] = input;
             }
+            displayWord = Object.values(wordOut).join("");
         }
         else {
             lives--;
             console.log(`Wrong. Lives ${lives}`);
+        }
+
+        if (!displayWord.includes("*")) {
+            won = true;
+            break;
         }
     }
     if (won) {
@@ -37,7 +47,7 @@ function main() {
 }
 
 function findIndexes(word, input) {
-   return [...word].reduce((indexArray, value, indexCurrent) => {
+    return [...word].reduce((indexArray, value, indexCurrent) => {
         if (value === input) {
             indexArray.push(indexCurrent);
         }
